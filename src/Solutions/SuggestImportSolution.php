57bda10ad993194ff2e5e504363dd89f0febc298
MrRio <james@parall.ax>
2019-09-02 00:17:13 +0100

    Stub out initial class import fixer code

--- src/Solutions/SuggestImportSolution.php
+++ src/Solutions/SuggestImportSolution.php
@@ -2,14 +2,14 @@
 
 namespace Facade\Ignition\Solutions;
 
-use Facade\IgnitionContracts\Solution;
+use Facade\IgnitionContracts\RunnableSolution;
 
-class SuggestImportSolution implements Solution
+class SuggestImportSolution implements RunnableSolution
 {
     /** @var string */
     protected $class;
 
-    public function __construct(string $class)
+    public function __construct(string $class = null)
     {
         $this->class = $class;
     }
@@ -21,11 +21,49 @@ class SuggestImportSolution implements Solution
 
     public function getSolutionDescription(): string
     {
-        return 'You have a missing class import. Try importing this class: `'.$this->class.'`.';
+        return '';
+    }
+
+    public function getSolutionActionDescription(): string
+    {
+        $output = [
+            'You have a missing class import. Try importing this class: `'.$this->class.'`.'
+        ];
+         return implode(PHP_EOL, $output);
+    }
+     public function getRunButtonText(): string
+    {
+        return 'Import class';
     }
 
     public function getDocumentationLinks(): array
     {
         return [];
+    }
+
+    public function getRunParameters(): array
+    {
+        return [
+            'class' => $this->class,
+        ];
+    }
+
+    public function isRunnable(array $parameters = [])
+    {
+        return false;
+        //return $this->makeOptional($this->getRunParameters()) !== false;
+    }
+
+    public function run(array $parameters = [])
+    {
+        $output = $this->importClass($parameters);
+        if ($output !== false) {
+            file_put_contents($parameters['viewFile'], $output);
+        }
+    }
+
+    public function importClass(array $parameters = [])
+    {
+        return false;
     }
 }
