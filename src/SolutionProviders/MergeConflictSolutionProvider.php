c7952fa4415c27138dd0af9aa766d9dbad7e2078
MrRio <james@parall.ax>
2019-09-02 20:21:01 +0100

    If we can't get branch name, fallback to 'current branch'

--- src/SolutionProviders/MergeConflictSolutionProvider.php
+++ src/SolutionProviders/MergeConflictSolutionProvider.php
@@ -38,12 +38,22 @@ class MergeConflictSolutionProvider implements HasSolutionsForThrowable
         $file = file_get_contents($throwable->getFile());
         preg_match('/\>\>\>\>\>\>\> (.*?)\n/', $file, $matches);
         $source = $matches[1];
-        $folder = basename($throwable->getFile());
-        $target = trim(`cd $folder; git branch | grep \* | cut -d ' ' -f2`);
 
+        $target = $this->getCurrentBranch(basename($throwable->getFile()));
         return [
-            BaseSolution::create("Merge conflict from branch '$source' into '$target'")
+            BaseSolution::create("Merge conflict from branch '$source' into $target")
                 ->setSolutionDescription('You have a Git merge conflict. To undo your merge do `git reset --hard HEAD`'),
         ];
+    }
+
+    private function getCurrentBranch($folder)
+    {
+        $branch = "'" . trim(`cd $folder; git branch | grep \* | cut -d ' ' -f2`) . "'";
+
+        if (! isset($branch) || $branch === "''") {
+            $branch = 'current branch';
+        }
+
+        return $branch;
     }
 }
