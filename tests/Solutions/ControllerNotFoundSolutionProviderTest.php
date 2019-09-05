cf524b24449f76d5edba945d31369fc7662527bf
Jeroen Deviaene <jeroen@deviaene.eu>
2019-09-05 14:47:42 +0200

    Tests for getting controller name from exception

---
+++ tests/Solutions/ControllerNotFoundSolutionProviderTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Facade\Ignition\Tests\Solutions;
+
+use ReflectionException;
+use Facade\Ignition\Tests\TestCase;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Facade\Ignition\SolutionProviders\ControllerNotFoundSolutionProvider;
+
+class ControllerNotFoundSolutionProviderTest extends TestCase
+{
+    /** @var ControllerNotFoundSolutionProvider */
+    private $provider;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->provider = new ControllerNotFoundSolutionProvider();
+    }
+
+    /** @test */
+    public function it_gets_controller_from_binding_resolution_exception()
+    {
+        $exception = new BindingResolutionException('Target class [App\Http\Controllers\Auth\LoginController] does not exist.');
+        $solutions = $this->provider->getSolutions($exception);
+
+        $this->assertStringContainsString('`php artisan make:controller Auth/LoginController`', $solutions[0]->getSolutionDescription());
+    }
+
+    /** @test */
+    public function it_gets_controller_from_reflection_exception()
+    {
+        $exception = new ReflectionException('Class App\Http\Controllers\MissingController does not exist');
+        $solutions = $this->provider->getSolutions($exception);
+
+        $this->assertStringContainsString('`php artisan make:controller MissingController`', $solutions[0]->getSolutionDescription());
+    }
+}
