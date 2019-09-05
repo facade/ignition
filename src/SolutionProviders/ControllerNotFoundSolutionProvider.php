3ccbcdb36f2a3f0a7d0af191cbde3c4335f70dd5
Jeroen Deviaene <jeroen@deviaene.eu>
2019-09-05 14:24:36 +0200

    Support ReflectionException for Laravel 5.x in ControllerNotFoundSolution

--- src/SolutionProviders/ControllerNotFoundSolutionProvider.php
+++ src/SolutionProviders/ControllerNotFoundSolutionProvider.php
@@ -3,6 +3,7 @@
 namespace Facade\Ignition\SolutionProviders;
 
 use Throwable;
+use ReflectionException;
 use Illuminate\Support\Str;
 use Facade\IgnitionContracts\HasSolutionsForThrowable;
 use Facade\Ignition\Solutions\CreateControllerSolution;
@@ -12,29 +13,43 @@ class ControllerNotFoundSolutionProvider implements HasSolutionsForThrowable
 {
     public function canSolve(Throwable $throwable): bool
     {
-        if (! $throwable instanceof BindingResolutionException) {
-            return false;
+        if ($throwable instanceof BindingResolutionException) {
+            return $this->isMissingControllerBindingError($throwable->getMessage());
         }
 
-        return  $this->isMissingControllerError($throwable->getMessage());
+        if ($throwable instanceof ReflectionException) {
+            return $this->isMissingControllerReflectionError($throwable->getMessage());
+        }
+
+        return false;
     }
 
-    protected function isMissingControllerError(string $message): bool
+    protected function isMissingControllerBindingError(string $message): bool
     {
         return Str::startsWith($message, 'Target class [App\\Http\\Controllers\\');
     }
 
+    protected function isMissingControllerReflectionError(string $message): bool
+    {
+        return Str::startsWith($message, 'Class App\Http\Controllers\\') && Str::endsWith($message, 'Controller does not exist');
+    }
+
     public function getSolutions(Throwable $throwable): array
     {
-        $controller = $this->getController($throwable->getMessage());
+        $controller = $this->getController($throwable);
 
         return [
             new CreateControllerSolution($controller),
         ];
     }
 
-    private function getController(string $message): string
+    private function getController(Throwable $throwable): string
     {
-        return preg_replace('/^.*\[(.*?)\].*$/', '$1', $message);
+        // Class App\Http\Controllers\SomeController does not exist
+        if ($throwable instanceof ReflectionException) {
+            return preg_replace('/^.*(App\\\\[^\s]+).*$/', '$1', $throwable->getMessage());
+        }
+
+        return preg_replace('/^.*\[(.*?)\].*$/', '$1', $throwable->getMessage());
     }
 }
