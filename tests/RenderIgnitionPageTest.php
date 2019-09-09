798935244d1055abad460fcde67ba2c9c6581af9
freek <freek@spatie.be>
2019-09-09 16:28:51 +0200

    add tests

---
+++ tests/RenderIgnitionPageTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Facade\Ignition\Tests;
+
+use Exception;
+use Illuminate\Support\Facades\Route;
+
+class RenderIgnitionPageTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        config()->set('app.debug', true);
+
+        Route::get('will-fail', function () {
+            throw new Exception('My exception');
+        });
+    }
+
+    /** @test */
+    public function when_requesting_html_it_will_respond_with_html()
+    {
+        /** @var \Illuminate\Http\Response $response */
+        $response = $this
+            ->get('will-fail')
+            ->baseResponse;
+
+        $this->assertStringStartsWith('text/html', $response->headers->get('Content-Type'));
+        $this->assertStringStartsWith('<!doctype html>', trim($response->getContent()));
+    }
+
+    /** @test */
+    public function when_requesting_json_it_will_respond_with_json()
+    {
+        /** @var \Illuminate\Http\Response $response */
+        $response = $this
+            ->getJson('will-fail');
+
+        $this->assertStringStartsWith('application/json', $response->headers->get('Content-Type'));
+        $this->assertEquals('My exception', json_decode($response->getContent(), true)['message']);
+    }
+}
