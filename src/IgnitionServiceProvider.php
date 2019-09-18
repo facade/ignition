e2aab65ac717868cad407a3852229738bdf7ac40
Tonko Mulder <Treggats@users.noreply.github.com>
2019-09-18 21:01:56 +0200

    added solution for running Laravel Dusk in production (#121)
    * added solution for running Laravel Dusk in production
    
    Signed-off-by: Tonko Mulder <tonko@tonkomulder.nl>
    
    * added tests for the laravel dusk exception
    
    Signed-off-by: Tonko Mulder <t.mulder@jvdmedia.nl>

--- src/IgnitionServiceProvider.php
+++ src/IgnitionServiceProvider.php
@@ -52,6 +52,7 @@ use Facade\Ignition\SolutionProviders\TableNotFoundSolutionProvider;
 use Illuminate\View\Engines\CompilerEngine as LaravelCompilerEngine;
 use Facade\Ignition\SolutionProviders\MissingPackageSolutionProvider;
 use Facade\Ignition\SolutionProviders\InvalidRouteActionSolutionProvider;
+use Facade\Ignition\SolutionProviders\RunningLaravelDuskInProductionProvider;
 use Facade\Ignition\SolutionProviders\IncorrectValetDbCredentialsSolutionProvider;
 use Facade\IgnitionContracts\SolutionProviderRepository as SolutionProviderRepositoryContract;
 
@@ -310,6 +311,7 @@ class IgnitionServiceProvider extends ServiceProvider
             InvalidRouteActionSolutionProvider::class,
             ViewNotFoundSolutionProvider::class,
             MergeConflictSolutionProvider::class,
+            RunningLaravelDuskInProductionProvider::class,
             MissingColumnSolutionProvider::class,
         ];
     }
