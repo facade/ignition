e62c9e6f7b815c35ac9f445791b5e93b0b12257b
MrRio <james@parall.ax>
2019-11-09 18:17:00 +0000

    Remove unused class imports

--- tests/Solutions/MissingImportSolutionProviderTest.php
+++ tests/Solutions/MissingImportSolutionProviderTest.php
@@ -2,15 +2,10 @@
 
 namespace Facade\Ignition\Tests\Solutions;
 
-use \Exception;
-use Illuminate\Support\Str;
-use UnexpectedValueException;
-use Facade\Ignition\Tests\TestCase;
-use Illuminate\Support\Facades\Route;
-use Facade\Ignition\Support\ComposerClassMap;
-use Symfony\Component\Debug\Exception\FatalThrowableError;
-use Facade\Ignition\Tests\stubs\Controllers\MissingClassController;
+use Exception;
 use Facade\Ignition\SolutionProviders\MissingImportSolutionProvider;
+use Facade\Ignition\Tests\stubs\Controllers\MissingClassController;
+use Facade\Ignition\Tests\TestCase;
 
 class MissingImportSolutionProviderTest extends TestCase
 {
@@ -34,6 +29,4 @@ class MissingImportSolutionProviderTest extends TestCase
         // $parameters = $solutions[0]->getRunParameters();
         // $this->assertTrue($solutions[0]->isRunnable($parameters));
     }
-
-
 }
