26d0eb10bbce821c3337037c3640b9408c454eb3
ManojKiran <manoj@postiefs.com>
2020-01-23 17:26:51 +0530

    added latest docs url

--- src/Solutions/RunMigrationsSolution.php
+++ src/Solutions/RunMigrationsSolution.php
@@ -27,7 +27,7 @@ class RunMigrationsSolution implements RunnableSolution
     public function getDocumentationLinks(): array
     {
         return [
-            'Database: Running Migrations docs' => 'https://laravel.com/docs/5.8/migrations#running-migrations',
+            'Database: Running Migrations docs' => 'https://laravel.com/docs/master/migrations#running-migrations',
         ];
     }
 
