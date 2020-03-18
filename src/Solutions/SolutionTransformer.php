c79c058d0dd5b6877eebfe25b81e62be0d72430b
Andrey Pokoev <nc101ux@gmail.com>
2020-03-18 22:20:04 +0300

    Fix execute solution route not definded. (#265)
    Fixed problem with action Facade\Ignition\Http\Controllers\ExecuteSolutionController not defined.

--- src/Solutions/SolutionTransformer.php
+++ src/Solutions/SolutionTransformer.php
@@ -5,6 +5,7 @@ namespace Facade\Ignition\Solutions;
 use Facade\IgnitionContracts\RunnableSolution;
 use Facade\IgnitionContracts\Solution;
 use Illuminate\Contracts\Support\Arrayable;
+use Throwable;
 
 class SolutionTransformer implements Arrayable
 {
@@ -29,7 +30,16 @@ class SolutionTransformer implements Arrayable
             'run_button_text' => $isRunnable ? $this->solution->getRunButtonText() : '',
             'run_parameters' => $isRunnable ? $this->solution->getRunParameters() : [],
             'action_description' => $isRunnable ? $this->solution->getSolutionActionDescription() : '',
-            'execute_endpoint' => action('\Facade\Ignition\Http\Controllers\ExecuteSolutionController'),
+            'execute_endpoint' => $this->executeEndpoint(),
         ];
+    }
+
+    protected function executeEndpoint(): string
+    {
+        try {
+            return action('\Facade\Ignition\Http\Controllers\ExecuteSolutionController');
+        } catch (Throwable $exception) {
+            return '';
+        }
     }
 }
