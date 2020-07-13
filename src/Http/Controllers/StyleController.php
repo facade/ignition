9f85928cee211fd95a851431ffce7571733f9e09
freekmurze <freekmurze@users.noreply.github.com>
2020-07-13 20:59:59 +0000

    Fix styling

--- src/Http/Controllers/StyleController.php
+++ src/Http/Controllers/StyleController.php
@@ -11,7 +11,8 @@ class StyleController
     {
         return response(
             file_get_contents(Ignition::styles()[$request->style]),
-            200, ['Content-Type' => 'text/css']
+            200,
+            ['Content-Type' => 'text/css']
         );
     }
 }
