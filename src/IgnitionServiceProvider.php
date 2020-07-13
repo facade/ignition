b5e6799104bcf0c0a8c9d665e33c5dc3acb0eff7
freek <freek@spatie.be>
2020-07-13 17:27:36 +0200

    Add ignition:make:solution-provider command

--- src/IgnitionServiceProvider.php
+++ src/IgnitionServiceProvider.php
@@ -5,6 +5,7 @@ namespace Facade\Ignition;
 use Facade\FlareClient\Flare;
 use Facade\FlareClient\Http\Client;
 use Facade\Ignition\Commands\SolutionMakeCommand;
+use Facade\Ignition\Commands\SolutionProviderMakeCommand;
 use Facade\Ignition\Commands\TestCommand;
 use Facade\Ignition\Context\LaravelContextDetector;
 use Facade\Ignition\DumpRecorder\DumpRecorder;
@@ -296,6 +297,7 @@ class IgnitionServiceProvider extends ServiceProvider
     {
         $this->app->bind('command.flare:test', TestCommand::class);
         $this->app->bind('command.make:solution', SolutionMakeCommand::class);
+        $this->app->bind('command.make:solution-provider', SolutionProviderMakeCommand::class);
 
         if ($this->app['config']->get('flare.key')) {
             $this->commands(['command.flare:test']);
@@ -303,6 +305,7 @@ class IgnitionServiceProvider extends ServiceProvider
 
         if ($this->app['config']->get('ignition.register_commands', false)) {
             $this->commands(['command.make:solution']);
+            $this->commands(['command.make:solution-provider']);
         }
 
         return $this;
