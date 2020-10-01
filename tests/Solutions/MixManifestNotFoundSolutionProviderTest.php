03b581acb6a91f88246ae436e566170a8a4bf3c7
Kevin McKee <mckee.kevin@gmail.com>
2020-10-01 18:00:34 -0500

    solve missing mix manifest error (#317)

---
+++ tests/Solutions/MixManifestNotFoundSolutionProviderTest.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Facade\Ignition\Tests\Solutions;
+
+use Exception;
+use Facade\Ignition\SolutionProviders\MissingMixManifestSolutionProvider;
+use Facade\Ignition\Tests\TestCase;
+use Illuminate\Support\Str;
+
+class MixManifestNotFoundSolutionProviderTest extends TestCase
+{
+    /** @test */
+    public function it_can_solve_a_missing_mix_manifest_exception()
+    {
+        $canSolve = app(MissingMixManifestSolutionProvider::class)
+            ->canSolve(new Exception('The Mix manifest does not exist.'));
+
+        $this->assertTrue($canSolve);
+    }
+
+    /** @test */
+    public function it_can_recommend_running_npm_install_and_npm_run_dev()
+    {
+        /** @var \Facade\IgnitionContracts\Solution $solution */
+        $solution = app(MissingMixManifestSolutionProvider::class)
+            ->getSolutions(new Exception('The Mix manifest does not exist.'))[0];
+
+        $this->assertTrue(Str::contains($solution->getSolutionDescription(), 'Did you forget to run `npm install && npm run dev`?'));
+    }
+}
