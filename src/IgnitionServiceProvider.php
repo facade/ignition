d35c1f428cf286302e9900de6ad34b9e8a5ec389
freek <freek@spatie.be>
2021-01-28 16:50:16 +0100

    import namespace

--- src/IgnitionServiceProvider.php
+++ src/IgnitionServiceProvider.php
@@ -62,6 +62,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\View\Engines\CompilerEngine as LaravelCompilerEngine;
 use Illuminate\View\Engines\PhpEngine as LaravelPhpEngine;
+use Livewire\CompilerEngineForIgnition;
 use Monolog\Logger;
 use Throwable;
 use Whoops\Handler\HandlerInterface;
@@ -131,8 +132,8 @@ class IgnitionServiceProvider extends ServiceProvider
         });
 
         $this->app->make('view.engine.resolver')->register('blade', function () {
-            if (class_exists(\Livewire\CompilerEngineForIgnition::class)) {
-                return new \Livewire\CompilerEngineForIgnition($this->app['blade.compiler']);
+            if (class_exists(CompilerEngineForIgnition::class)) {
+                return new CompilerEngineForIgnition($this->app['blade.compiler']);
             }
 
             return new CompilerEngine($this->app['blade.compiler']);
