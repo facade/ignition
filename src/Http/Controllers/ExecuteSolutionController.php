001b354182c98a0595526231ae3b439e26168fd5
AlexVanderbist <AlexVanderbist@users.noreply.github.com>
2021-03-30 08:12:40 +0000

    Fix styling

--- src/Http/Controllers/ExecuteSolutionController.php
+++ src/Http/Controllers/ExecuteSolutionController.php
@@ -36,7 +36,7 @@ class ExecuteSolutionController
         $ipIsPublic = filter_var(
             request()->ip(),
             FILTER_VALIDATE_IP,
-            FILTER_FLAG_IPV4 | FILTER_FLAG_NO_PRIV_RANGE |  FILTER_FLAG_NO_RES_RANGE
+            FILTER_FLAG_IPV4 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE
         );
 
         if ($ipIsPublic) {
