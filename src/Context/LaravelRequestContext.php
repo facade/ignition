a6f9fcd06c47074cbbe9534c51b2c8bc371e56ca
freek <freek@spatie.be>
2021-04-07 23:08:15 +0200

    import namespace

--- src/Context/LaravelRequestContext.php
+++ src/Context/LaravelRequestContext.php
@@ -4,6 +4,7 @@ namespace Facade\Ignition\Context;
 
 use Facade\FlareClient\Context\RequestContext;
 use Illuminate\Http\Request;
+use Throwable;
 
 class LaravelRequestContext extends RequestContext
 {
@@ -23,7 +24,7 @@ class LaravelRequestContext extends RequestContext
             if (! $user) {
                 return [];
             }
-        } catch (\Throwable $e) {
+        } catch (Throwable $e) {
             return [];
         }
 
@@ -35,7 +36,7 @@ class LaravelRequestContext extends RequestContext
             if (method_exists($user, 'toArray')) {
                 return $user->toArray();
             }
-        } catch (\Throwable $e) {
+        } catch (Throwable $e) {
             return [];
         }
 
@@ -70,7 +71,7 @@ class LaravelRequestContext extends RequestContext
     {
         try {
             return collect(optional($this->request->route())->parameters ?? [])->toArray();
-        } catch (\Throwable $e) {
+        } catch (Throwable $e) {
             return [];
         }
     }
