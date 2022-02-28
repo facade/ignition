169041261f894f611b948fd5ba21e7169bfda5e8
viest <wjx@php.net>
2022-02-28 10:12:51 +0800

    Update CollectsViewExceptions.php

--- src/Views/Concerns/CollectsViewExceptions.php
+++ src/Views/Concerns/CollectsViewExceptions.php
@@ -21,6 +21,9 @@ trait CollectsViewExceptions
     
     public function flushViewData(): void
     {
+        // If we need to render multiple views at the same time, 
+        // $lastCompiledData will hold all the view data, which can lead to excessive memory usage.
+        // We can flush the view data so that we can prevent memory transitions and also improve retrieval efficiency.
         $this->lastCompiledData = [];
     }
 
